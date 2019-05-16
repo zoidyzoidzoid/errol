a190973356348bbadbceba8bd30c6123a1631251
William Stewart <zoidbergwill@gmail.com>
2019-05-16 15:51:13 +0200

    Fix opencensus and net/http/pprof support

--- go/pkg/server/server.go
+++ go/pkg/server/server.go
@@ -20,10 +20,12 @@ import (
 	"net/http"
 
 	// pprof for profiling
-	_ "net/http/pprof"
+	"net/http/pprof"
 	"time"
 
 	"go.opencensus.io/plugin/ochttp"
+	"go.opencensus.io/plugin/ochttp/propagation/b3"
+	"go.opencensus.io/zpages"
 )
 
 const (
@@ -36,19 +38,41 @@ const (
 	GitlabEventHookPushValue = "Push Hook"
 
 	EventTypePush = "push"
+	addr          = ":8082"
 )
+
+// var (
+// // zpagesAddr = os.Getenv("PROF_HTTP", ":8082", "net/http/pprof http bind address.")
+// // profAddr = env.Get("ERROL_PROF_HTTP", ":8082", "net/http/pprof http bind address.")
+// 	// profAddr = os.Getenv("ERROL_PROF_HTTP")
+// )
 
 // Run should have a comment
 func Run() {
+	// https://medium.com/observability/debugging-latency-in-go-1-11-9f97a7910d68
+	go func() {
+		pp := http.NewServeMux()
+		pp.Handle("/debug/pprof/", http.HandlerFunc(pprof.Index))
+		pp.Handle("/debug/pprof/cmdline", http.HandlerFunc(pprof.Cmdline))
+		pp.Handle("/debug/pprof/profile", http.HandlerFunc(pprof.Profile))
+		pp.Handle("/debug/pprof/symbol", http.HandlerFunc(pprof.Symbol))
+		pp.Handle("/debug/pprof/trace", http.HandlerFunc(pprof.Trace))
+		zpages.Handle(pp, "/debug/zpages")
+		log.Println("warning: could not start debug HTTP server:", http.ListenAndServe(addr, pp))
+	}()
+
 	gitHubHandler := GitHubHandler{}
 	gitlabHandler := GitlabHandler{}
 	http.Handle("/api/v1/hooks/github", ochttp.WithRouteTag(gitHubHandler, "/api/v1/hooks/github"))
 	http.Handle("/api/v1/hooks/gitlab", ochttp.WithRouteTag(gitlabHandler, "/api/v1/hooks/gitlab"))
 	srv := &http.Server{
 		Addr:         ":8080",
+		Handler: &ochttp.Handler{
+			Propagation: &b3.HTTPFormat{},
+		},
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
-	fmt.Printf("Starting server on %s\n", srv.Addr)
+	fmt.Printf("Starting server on http://127.0.0.1%s\n", srv.Addr)
 	log.Fatal(srv.ListenAndServe())
 }
